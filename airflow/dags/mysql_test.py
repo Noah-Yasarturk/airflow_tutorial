4d1691bf71222ef402ebb0a01f0f0a8baf7f7bec
Noah Yasarturk <Noah.Yasarturk@gmail.com>
2021-09-09 15:11:30 -0400

    Successful mysql call, but should not be manipulating data in airflow itself. Airflow is an orchestrator

--- airflow/dags/mysql_test.py
+++ airflow/dags/mysql_test.py
@@ -11,7 +11,7 @@ my_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=2)
 }
 
 # Dag definition
@@ -24,24 +24,51 @@ with DAG(
     tags = ['custom']
 ) as dag:
 
-    def get_conn_id():
-        path = '/opt/airflow/sens.txt'
-        sens_contents = ''
-        with open(path, 'r') as f:
-            sens_contents = f.read()
-            print('Contents of sens: ' + sens_contents)
-        
+    # def get_conn_id():
+    #     path = '/opt/airflow/sens.txt'
+    #     sens_contents = ''
+    #     with open(path, 'r') as f:
+    #         sens_contents = f.read()
+    #         print('Contents of sens: ' + sens_contents)
+    #     # Create dict from results
+    #     d = {}
+    #     for l in sens_contents.split('\n'):
+    #         s = l.split(':')
+    #         d[s[0]] = s[1]
+    #     # Using dict, create connection string
 
+    def get_records(**kwargs):
+        '''Pulls the results from the query_task, prints, and returns'''
+        ti = kwargs['ti']
+        xcom = ti.xcom_pull(task_ids='query_task')
+        string_out = 'Value pulled from MySQL: {}'.format(xcom)
+        print(string_out)
+        return string_out
 
-    printable_task = PythonOperator(
-        task_id = 'print_conn_id',
-        python_callable=get_conn_id
+    # Connect & query
+    mysql_task = MySqlOperator(
+        task_id = 'query_task',
+        mysql_conn_id = 'dev_playground',
+        sql = 'SELECT * FROM test'
     )
 
-    # Connect
-    # mysql_task = MySqlOperator(
-    #     task_id = 'query_task',
-    #     mysql_conn_id = get_conn_id()
-    # )
+    # Results will be in xcom. Pull them
+    printable_task = PythonOperator(
+        task_id = 'print_records',
+        python_callable=get_records
+    )
 
-    printable_task
+    printable_task.doc_md = ''' In this case, 
+    printable_task printed no records.
+    Airflow is an orchestration tool, not a data manipulation
+    tool. The work should be done by the files it executes.
+    It is not like Apache NiFi in this regard.
+
+    Data is not to be passed between tasks. Tasks are atomic. 
+    How then should we do conditional logic?? Next branch.
+    '''
+
+    mysql_task >> printable_task
+
+
+    

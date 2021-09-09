bc03ba817d49eab158f8c8aa061ccb56d7ca3774
Noah Yasarturk <Noah.Yasarturk@gmail.com>
2021-09-09 15:41:59 -0400

    Pulled in custom processor that overrides MySqlOperator to return results

--- airflow/dags/mysql_test.py
+++ airflow/dags/mysql_test.py
@@ -1,8 +1,10 @@
 from airflow import DAG
 from datetime import timedelta
-from airflow.operators.mysql_operator import MySqlOperator
-from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago 
+from airflow.operators.mysql_operator import MySqlOperator as BaseMySqlOperator
+from airflow.hooks.mysql_hook import MySqlHook
+from airflow.operators.python import PythonOperator
+
 
 my_args = {
     'owner': 'airflow',
@@ -24,20 +26,20 @@ with DAG(
     tags = ['custom']
 ) as dag:
 
-    # def get_conn_id():
-    #     path = '/opt/airflow/sens.txt'
-    #     sens_contents = ''
-    #     with open(path, 'r') as f:
-    #         sens_contents = f.read()
-    #         print('Contents of sens: ' + sens_contents)
-    #     # Create dict from results
-    #     d = {}
-    #     for l in sens_contents.split('\n'):
-    #         s = l.split(':')
-    #         d[s[0]] = s[1]
-    #     # Using dict, create connection string
+    class ReturningMySqlOperator(BaseMySqlOperator):
+        '''
+        The normal MySqlOperator does not return the data that it queries. This one does. 
+        From https://stackoverflow.com/questions/52645905/how-to-use-mysqloperator-with-xcom-in-airflow
+        '''
+        def execute(self, context):
+            self.log.info('Executing: %s', self.sql) # log the query being run
+            hook = MySqlHook(mysql_conn_id=self.mysql_conn_id,
+                            schema=self.database)
+            return hook.get_first(
+                self.sql,
+                parameters=self.parameters)
 
-    def get_records(**kwargs):
+    def print_records(**kwargs):
         '''Pulls the results from the query_task, prints, and returns'''
         ti = kwargs['ti']
         xcom = ti.xcom_pull(task_ids='query_task')
@@ -46,16 +48,17 @@ with DAG(
         return string_out
 
     # Connect & query
-    mysql_task = MySqlOperator(
+    mysql_task = ReturningMySqlOperator(
         task_id = 'query_task',
         mysql_conn_id = 'dev_playground',
-        sql = 'SELECT * FROM test'
+        sql = 'SELECT COUNT(*) c FROM test',
+        # do_xcom_push=True # MySqlOperator does not support do_xcom_push
     )
 
     # Results will be in xcom. Pull them
     printable_task = PythonOperator(
         task_id = 'print_records',
-        python_callable=get_records
+        python_callable=print_records
     )
 
     printable_task.doc_md = ''' In this case, 
